b0917e31ee6c702613eaf1cd4e81059980c6c5cc
Chin Sek Yi <sekyichin@gmail.com>
2024-06-02 00:24:11 +0800

    remove irrelevant files

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,7 @@
 import sys
+
 from logger import logging
+
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
@@ -21,9 +23,10 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
 
+
 if __name__ == "__main__":
     try:
-        a = 1/0
+        a = 1 / 0
     except Exception as e:
         logging.error("divide by zero")
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
