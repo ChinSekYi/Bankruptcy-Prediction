b13ca7313b10da91d9de79bd26b0e6688880a9aa
Chin Sek Yi <sekyichin@gmail.com>
2024-06-17 23:46:23 +0800

    data transformation

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,5 +1,5 @@
-import sys
 import os
+import sys
 from dataclasses import dataclass
 from pathlib import Path
 
@@ -8,7 +8,7 @@ import pandas as pd
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder, StandardScaler
 
 project_root = Path(__file__).resolve().parents[2]
 sys.path.append(str(project_root))
@@ -17,5 +17,45 @@ from src.exception import CustomException
 from src.logger import logging
 
 
+@dataclass
 class DataTransformationConfig:
-    preprocessor_ob_file_path = os.path.join('artefacts', 'preprocessor.pk1')
+    preprocessor_ob_file_path = os.path.join("artifacts", "preprocessor.pk1")
+
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config = DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        try:
+            numerical_columns = [i for i in range(0, 65)]
+            categorical_columns = [65]
+            num_pipeline = Pipeline(
+                steps=[
+                    ("imputer", SimpleImputer(strategy="median")),
+                    ("scaler", MinMaxScaler()),
+                ]
+            )
+
+            """
+            cat_pipeline = Pipeline(
+                steps=[]
+                ]
+            )
+            """
+            logging.info(f"Categorical columns: {categorical_columns}")
+            logging.info(f"Numerical columns: {numerical_columns}")
+
+            preprocessor = ColumnTransformer(
+                [
+                    ("num_pipeline"),
+                    num_pipeline,
+                    numerical_columns,
+                    # ,("cat_pipeline", cat_pipeline, categorical_columns)
+                ]
+            )
+
+            return preprocessor
+
+        except Exception as e:
+            raise CustomException(e, sys)
--- src/components/test.py
+++ src/components/test.py
@@ -6,10 +6,9 @@ from pathlib import Path
 project_root = Path(__file__).resolve().parents[2]
 sys.path.append(str(project_root))
 
-print(os.path.abspath("src"))
-print(os.listdir(project_root))
-
 from src.exception import CustomException
 from src.logger import logging
 
 print("works")
+
+print([i for i in range(0, 64)])
