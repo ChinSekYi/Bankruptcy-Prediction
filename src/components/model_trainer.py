166590209b4266a5d834114b90aceda6659092d9
Chin Sek Yi <sekyichin@gmail.com>
2024-06-20 01:05:37 +0800

    fix bug

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -75,6 +75,7 @@ class ModelTrainer:
                     "n_estimators": [8, 16, 32, 64, 128, 256],
                 },
                 "Linear Regression": {},
+                "Logistic Regression": {},
                 "XGBRegressor": {
                     "learning_rate": [0.1, 0.01, 0.05, 0.001],
                     "n_estimators": [8, 16, 32, 64, 128, 256],
