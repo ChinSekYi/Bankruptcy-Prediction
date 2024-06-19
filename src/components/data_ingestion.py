92d49059b7d6633951adf774f248f9388b4ac71b
Chin Sek Yi <sekyichin@gmail.com>
2024-06-19 20:59:11 +0800

    update data_ingetion and model_trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -13,6 +13,7 @@ sys.path.append(str(project_root))
 
 from src.components.data_transformation import (DataTransformation,
                                                 DataTransformationConfig)
+from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 from src.exception import CustomException
 from src.logger import logging
 
@@ -72,3 +73,6 @@ if __name__ == "__main__":
     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(
         train_data, test_data
     )
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -28,7 +28,7 @@ class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
 
-    def initiate_model_trainer(self, train_array, test_array, preprocessor_path):
+    def initiate_model_trainer(self, train_array, test_array):
         try:
             logging.info("Splitting training and test input data")
             X_train, y_train, X_test, y_test = (
@@ -51,33 +51,32 @@ class ModelTrainer:
             }
 
             model_report: dict = evaluate_model(
-                X=X_train, y=y_train, x_test=X_test, y_test=y_test, models=models
+                X=X_train, y_train=y_train, X_test=X_test, y_test=y_test, models=models
             )
 
             best_model_score = max(model_report.values())
 
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
-                ]
-            
+            ]
+
             best_model = models[best_model_name]
 
-            if best_model_score<0.6:
+            if best_model_score < 0.6:
                 raise CustomException("No best model found")
-            
+
             logging.info(f"Best model on both training and testing dataset")
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
-                obj = best_model
+                obj=best_model,
             )
 
-            predicted=best_model.predict(X_test)
+            predicted = best_model.predict(X_test)
 
-            r2_score_value=r2_score(y_test, predicted)
+            r2_score_value = r2_score(y_test, predicted)
 
             return r2_score_value
-
 
         except Exception as e:
             raise CustomException(e, sys)
