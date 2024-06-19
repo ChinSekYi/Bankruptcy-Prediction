dd39154fbc005280731f9265aa58dbe762164a82
Chin Sek Yi <sekyichin@gmail.com>
2024-06-20 01:00:29 +0800

    add parameters

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -59,12 +59,44 @@ class ModelTrainer:
                 "AdaBoosting Classifier": AdaBoostRegressor(),
             }
 
+            params = {
+                "Decision Tree": {
+                    "criterion": [
+                        "squared_error",
+                        "friedman_mse",
+                        "absolute_error",
+                        "poisson",
+                    ],
+                },
+                "Random Forest": {"n_estimators": [8, 16, 32, 64, 128, 256]},
+                "Gradient Boosting": {
+                    "learning_rate": [0.1, 0.01, 0.5, 0.01],
+                    "subsample": [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+                "Linear Regression": {},
+                "XGBRegressor": {
+                    "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+                "CatBoostRegressor": {
+                    "depth": [6, 9, 10],
+                    "learning_rate": [0.01, 0.05, 0.1],
+                    "iterations": [30, 50, 100],
+                },
+                "AdaBoost Regressor": {
+                    "learning_rate": [0.1, 0.01, 0.5, 0.001],
+                    "n_estimators": [8, 16, 32, 64, 128, 256],
+                },
+            }
+
             model_report: dict = evaluate_model(
                 X_train=X_train,
                 y_train=y_train,
                 X_test=X_test,
                 y_test=y_test,
                 models=models,
+                params=params,
             )
 
             best_model_score = max(model_report.values())
