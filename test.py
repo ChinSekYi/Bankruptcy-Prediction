42601f6722c999efed242635f07cef22de785695
Chin Sek Yi <sekyichin@gmail.com>
2024-06-20 21:33:24 +0800

    fix bugs + add test

--- test.py
+++ test.py
@@ -3,11 +3,79 @@ import os
 import sys
 from datetime import datetime
 from src.utils import save_object, load_object
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+preprocessor = load_object(file_path=os.path.join("artifacts", "preprocessor.pkl"))
+print(preprocessor)
+data = CustomData(
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+    29,
+    30,
+    31,
+    32,
+    33,
+    34,
+    35,
+    36,
+    37,
+    38,
+    39,
+    40,
+    41,
+    42,
+    43,
+    44,
+    45,
+    46,
+    47,
+    48,
+    49,
+    50,
+    51,
+    52,
+    53,
+    54,
+    55,
+    56,
+    57,
+    58,
+    59,
+    60,
+    61,
+    62,
+    63,
+    64,
+)
 
-#load_object('/Users/sekyichin/Downloads/Bankruptcy-Prediction/artifacts/model.pkl')
-load_object('artifacts/preprocessor.pkl')
-#load_object('artifacts\\model.pkl') s
+pred_df = data.get_data_as_dataframe()
+print(pred_df)
+
+predict_pipeline = PredictPipeline()
+results = predict_pipeline.predict(pred_df)
