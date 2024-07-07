9fb789439594708d62195ce65c56c565d50d3017
Chin Sek Yi <sekyichin@gmail.com>
2024-07-08 06:49:53 +0800

    add new test

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
+
 
 def test_data_ingestion_output():
     obj = DataIngestion()
@@ -16,8 +18,18 @@ def test_data_transformation_output():
     train_path = "artifacts/train.csv"
     test_path = "artifacts/test.csv"
 
-    train_arr, test_arr, preprocessor_path = obj.initiate_data_transformation(train_path, test_path)
+    train_arr, test_arr, preprocessor_path = obj.initiate_data_transformation(
+        train_path, test_path
+    )
 
     assert train_arr is not None
     assert test_arr is not None
     assert preprocessor_path is not None
+
+
+def test_model_output():
+    obj = ModelTrainer()
+    train_array = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
+    test_array = [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]
+    obj.initiate_model_trainer(train_array, test_array)
+    assert obj.model_trainer_config.trained_model_file_path == "artifacts/model.pkl"
