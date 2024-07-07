336eea369bd6b97f1f13a50581f4a8cc4de0667c
Chin Sek Yi <sekyichin@gmail.com>
2024-07-08 06:51:29 +0800

    update new test

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
 from src.components.model_trainer import ModelTrainer
 
-
 def test_data_ingestion_output():
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
@@ -18,9 +17,7 @@ def test_data_transformation_output():
     train_path = "artifacts/train.csv"
     test_path = "artifacts/test.csv"
 
-    train_arr, test_arr, preprocessor_path = obj.initiate_data_transformation(
-        train_path, test_path
-    )
+    train_arr, test_arr, preprocessor_path = obj.initiate_data_transformation(train_path, test_path)
 
     assert train_arr is not None
     assert test_arr is not None
@@ -31,5 +28,8 @@ def test_model_output():
     obj = ModelTrainer()
     train_array = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]
     test_array = [[11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]
-    obj.initiate_model_trainer(train_array, test_array)
+    predicted, r2_score_value = obj.initiate_model_trainer(train_array, test_array)
+
     assert obj.model_trainer_config.trained_model_file_path == "artifacts/model.pkl"
+    assert predicted.isnumeric()
+    assert r2_score_value.isnumeric()
